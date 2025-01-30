cbd225fae8c844faab2e3b4ac8a49db2116b6291
0mytimewillcome0 <0my.time.will.come0@gmail.com>
2025-01-30 19:05:31 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+making_breakfast = True
+eggs_are_done = True
+if making_breakfast:  # Check if you're making breakfast
+    print("Start cooking eggs")  # This happens only if you're making breakfast
+if eggs_are_done:  # Check if the eggs are done
+        print("Serve on a plate")  # This happens only if eggs are done
+
+print("Enjoy your breakfast!")  # This happens after everything
